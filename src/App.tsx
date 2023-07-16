682de93e296e04875e46c11d99069c31853dbf61
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-07-16 14:41:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+        <AppTitle />
+        <Rating />
+        <Accordion />
+        <Rating />
+    </div>
+  );
+}
+
+function AppTitle() {
+    return <> This is App </>
+}
+
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+)
+}
+
+
+function Star() {
+    return (
+        <div>
+            <div>star</div>
+        </div>
+    );
+};
+
+
+
+function Accordion() {
+    return (
+        <div>
+            <AccordionTitle />
+            <AccordionBody />
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+       <div>
+           <h3>Menu</h3>
+       </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+export default App;
